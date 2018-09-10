9ce76557aa60ee08cffcda0db1550067b94efaa1
hasanarofid <hasanarofid@gmail.com>
2018-09-10 13:39:11 +0700

    hasil implementasi form pada vue js

---
+++ apps.js
@@ -0,0 +1,32 @@
+new Vue({
+  el: "#app",
+  data: {
+    pesan: ""
+  }
+});
+
+new Vue({
+  el: "#app2",
+  data: {
+    pesan2: ""
+  }
+});
+
+new Vue({
+  el: "#app3",
+  data: {
+    pilih: ""
+  }
+});
+new Vue({
+  el: "#app4",
+  data: {
+    ceklist: []
+  }
+});
+new Vue({
+  el: "#app5",
+  data: {
+    kota: ""
+  }
+});
